24758257c5619869ce17e4f67b46407dec10d245
Perseus29 <priyanshumahawar29@gmail.com>
2024-04-04 19:41:32 +0530

    initial

---
+++ food_classification_model.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void food_classification_model(k2c_tensor* input_1_input, k2c_tensor* activation_output); 
+void food_classification_model_initialize(); 
+void food_classification_model_terminate(); 
